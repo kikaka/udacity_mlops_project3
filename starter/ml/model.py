69b3c5028479c613596a2f5f5ac9339a9e712b76
kikaka <kayauerbach@web.de>
2022-02-18 21:50:13 +0100

    Implements methods in model.py and calls in train_model.py

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,3 +1,4 @@
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 
 
@@ -17,8 +18,11 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-
-    pass
+    # Init logistic regression model
+    model = LogisticRegression()
+    # train model
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +61,5 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    preds = model.predict(X)
+    return preds
--- starter/train_model.py
+++ starter/train_model.py
@@ -1,10 +1,14 @@
 # Script to train machine learning model.
+import os
+import pickle
+
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference
 
 # load data from census_cleaned.csv
-data = pd.read_csv('data/census_cleaned.csv')
+data = pd.read_csv('../data/census_cleaned.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -25,6 +29,14 @@ X_train, y_train, encoder, lb = process_data(
 
 # Proces the test data with the process_data function.
 X_test, y_test, encoder_test, lb_test = process_data(
-    test, categorical_features=cat_features, label="salary", training=True
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
 )
 # Train and save a model.
+model = train_model(X_train, y_train)
+pickle.dump(model, open('../model/logistic_regression_model.pkl', 'wb'))
+
+predictions = inference(model, X_test)
+model_precision, model_recall, model_fbeta = compute_model_metrics(y_test, predictions)
+print('precision: ', model_precision)
+print('recall: ', model_recall)
+print('fbeta: ', model_fbeta)
